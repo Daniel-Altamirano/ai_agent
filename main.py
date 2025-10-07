f41a93901e80ca638f982d045d7ce3a159418136
Daniel Altamirano <altamirano.daniel@gmail.com>
2025-10-07 09:58:55 -0700

    added system_prompt

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
 
 
 def main():
@@ -22,16 +23,18 @@ def main():
 
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    model_name = 'gemini-2.0-flash-001'
 
     user_prompt = " ".join(args)
 
     messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
-    
+   
     def generate_content(client, messages, verbose):
 
         response = client.models.generate_content(
-            model='gemini-2.0-flash-001',
+            model=model_name,
             contents=messages,
+            config=types.GenerateContentConfig(system_instruction=system_prompt),
         )
 
         if verbose:
