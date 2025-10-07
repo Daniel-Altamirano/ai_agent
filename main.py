2f2922141873b8b5f0fb282c46ea85f6f600d0c8
Daniel Altamirano <altamirano.daniel@gmail.com>
2025-10-06 17:10:38 -0700

    added verbose request option

--- main.py
+++ main.py
@@ -8,31 +8,42 @@ from google.genai import types
 def main():
     load_dotenv()
     
-    args = sys.argv[1:]
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
 
     if not args:
         print("AI Code Assistant")
-        print('\nUsage: python main.py "your prompt here"')
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
-    user_prompt = " ".join(args)
 
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    user_prompt = " ".join(args)
+
     messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
     
-    response = client.models.generate_content(
-        model='gemini-2.0-flash-001',
-        contents=messages,
-    )
+    def generate_content(client, messages, verbose):
 
+        response = client.models.generate_content(
+            model='gemini-2.0-flash-001',
+            contents=messages,
+        )
 
+        if verbose:
+            print(f"User prompt: {user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+        print("Response:")
+        print(response.text)
+
+    generate_content(client=client, messages=messages, verbose=verbose)
     
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print("Response:")
-    print(response.text)
 
 if __name__ == "__main__":
     main()
