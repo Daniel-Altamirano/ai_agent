51fd4ca3fb5120775b394e5d340860d4f0707cc1
Daniel Altamirano <altamirano.daniel@gmail.com>
2025-10-06 19:44:21 -0700

    refactor

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,29 +1,27 @@
 import os
 
 def get_files_info(working_directory, directory="."):
-    working_directory = os.path.realpath(os.path.expanduser(working_directory))
-    full_path = os.path.realpath(os.path.join(working_directory, directory))
-    #print(f"{full_path=}")
+    working_dir = os.path.realpath(os.path.expanduser(working_directory))
+    target_dir = os.path.realpath(os.path.join(working_dir, directory))
 
-    if not os.path.commonpath([working_directory, full_path]) == working_directory:
+    if not os.path.commonpath([working_dir, target_dir]) == working_dir:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
-    if not os.path.isdir(full_path):
+    if not os.path.isdir(target_dir):
         return f'Error: "{directory}" is not a directory'
 
+    try:
+        contents = os.listdir(target_dir)
+        contents_info = []
 
-    contents = os.listdir(full_path)
-    #print(f"{contents=}")
-    contents_info = []
+        for item in contents:
+            item_path = os.path.join(target_dir, item)
+            file_size = os.path.getsize(item_path)
+            is_dir = os.path.isdir(item_path)
+            contents_info.append(f"- {item}: {file_size=} bytes, {is_dir=}")
 
-    for item in contents:
-        item_path = os.path.join(full_path, item)
-        #print(f"{item_path=}")
-        file_size = os.path.getsize(item_path)
-        is_dir = os.path.isdir(item_path)
-        contents_info.append(f"- {item}: {file_size=} bytes, {is_dir=}")
-
-    contents_info = "\n".join(contents_info)
-    #print(f"{contents_info=}")
-    return contents_info
+        contents_info = "\n".join(contents_info)
+        return contents_info
+    except Exception as e:
+        return f"Error listing files: {e}"
     
