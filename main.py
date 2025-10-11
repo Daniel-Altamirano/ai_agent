202a7039c437f8756eca6e8d0ead5ed0047ed775
Daniel Altamirano <altamirano.daniel@gmail.com>
2025-10-10 18:08:37 -0700

    agent can make multiple function calls

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import os
 import sys
-from call_function import available_functions
-from call_function import call_function
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from prompts import system_prompt
+
+from call_function import available_functions, call_function
 from functions.get_files_info import *
+from functions.config import MAX_ITERS
+from prompts import system_prompt
 
 
 def main():
@@ -26,42 +27,73 @@ def main():
 
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    model_name = 'gemini-2.0-flash-001'
 
     user_prompt = " ".join(args)
 
-    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
+    if verbose: 
+        print(f"User prompt: {user_prompt}\n")
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+        ]
+
+    iters = 0
+    while True:
+        iters += 1
+        if iters > MAX_ITERS:
+            print(f"Maximum iterations ({MAX_ITERS}) reached.")
+            sys.exit(1)
+
+        try:
+            final_response = generate_content(client=client, messages=messages, verbose=verbose)
+            if final_response:
+                print("Final response:")
+                print(final_response)
+                break
+        except Exception as e:
+            print(f"Error in generate_content: {e}")
 
    
-    def generate_content(client, messages, verbose):
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001',
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt,
+        ),
+    )
 
-        response = client.models.generate_content(
-            model=model_name,
-            contents=messages,
-            config=types.GenerateContentConfig(
-                tools=[available_functions],
-                system_instruction=system_prompt,
-            ),
-        )
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
+    if response.candidates:
+        for candidate in response.candidates:
+            function_call_content = candidate.content
+            messages.append(function_call_content)
+
+    if not response.function_calls:
+        return response.text
+    
+    function_responses = []
+    for function_call_part in response.function_calls:
+        function_call_result = call_function(function_call_part, verbose)
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("empty function call result")
+        
         if verbose:
-            print(f"User prompt: {user_prompt}")
-            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        function_responses.append(function_call_result.parts[0])
 
-        print("Response:")
-        print(response.text)
-        if response.function_calls:
-            for function_call_part in response.function_calls:
-                #print(f"Calling function: {function_call_part.name}({function_call_part.args})")
-                function_call_result = call_function(function_call_part, verbose)
-                try:
-                    print(f"-> {function_call_result.parts[0].function_response.response}")
-                except Exception as e:
-                    print(f"Error: {e}")
-                    sys.exit(1)
+    if not function_responses:
+        raise Exception("no function reponses generated -> exiting")
+    
+    messages.append(types.Content(role="user", parts=function_responses))
 
-    generate_content(client=client, messages=messages, verbose=verbose)
     
 
 if __name__ == "__main__":
